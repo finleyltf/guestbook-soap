9263ba13f61f877c4f3bc8e687034c11ca03b685
Tefu Lin <finleyltf@gmail.com>
2014-03-13 11:25:15 +0200

    repo init

---
+++ module/Post/src/Post/Serivce/ServiceAPI.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Post\Service;
+
+
+class ServiceAPI
+{
+
+    /**
+     * @return string a string result
+     */
+    public function testFunction()
+    {
+
+        return 'this is a test function!33333';
+
+    }
+
+
+}
